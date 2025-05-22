91d431dfbc3bc8e5f96e86126fbc64c410d5e99b
devyani-keche <devyanik2006@gmail.com>
2025-05-23 04:08:45 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './app.css'; // Import Tailwind CSS here
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
